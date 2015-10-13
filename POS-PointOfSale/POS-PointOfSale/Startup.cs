c6d01db378f79d4c0c39d348ee3ea628378eeeb6
Yuuki16 <yuuki.monge16@gmail.com>
2015-10-13 11:55:44 -0600

    "Proyecto"

---
+++ POS-PointOfSale/POS-PointOfSale/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(POS_PointOfSale.Startup))]
+namespace POS_PointOfSale
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
