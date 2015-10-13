c5ab3fadc71aa5e17f2733519ab9f505ad926f8d
brygondu20 <bryangonzalez20@hotmail.com>
2015-10-13 12:41:35 -0600

    Cliente1.0

---
+++ POS-PointOfSale/POS-PointOfSale/Models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace POS_PointOfSale.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int cl_cedula { get; set; }
+
+        public DateTime cl_fechaCreacion { get; set; }
+
+        public string cl_activo { get; set; }
+
+        public int cl_direccion { get; set; }
+
+        public int cl_tel { get; set; }
+    }
+}
