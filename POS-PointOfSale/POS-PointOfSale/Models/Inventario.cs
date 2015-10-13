2f9fcedfbd4f38af596f156200b2a4e0d7617e1f
brygondu20 <bryangonzalez20@hotmail.com>
2015-10-13 17:10:29 -0600

    POSWeb1.0

---
+++ POS-PointOfSale/POS-PointOfSale/Models/Inventario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace POS_PointOfSale.Models
+{
+    public class Inventario
+    {
+        [Key]
+        public int in_codArticulo { get; set; }
+        
+        [Display(Name = "Descripción")]
+        [Required(ErrorMessage = "Debe ingresar {0}")]
+        [StringLength(30, ErrorMessage = "El campo {0} debe estar entre {2} and {1} caracteres", MinimumLength = 3)]
+        public string in_descripcion { get; set; }
+
+        [Display(Name = "Cantidad")]
+        [Required(ErrorMessage = "Debe ingresar {0}")]
+        [DisplayFormat(DataFormatString = "{0:P2}", ApplyFormatInEditMode = false)]
+        public float in_cantidad { get; set; }
+
+        [Display(Name = "Costo")]
+        [Required(ErrorMessage = "Debe ingresar {0}")]
+        [DisplayFormat(DataFormatString="{0:C2}",ApplyFormatInEditMode = false)]
+        public decimal in_costo { get; set; }
+
+        [Display(Name = "Precio")]
+        [Required(ErrorMessage = "Debe ingresar {0}")]
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal in_precio { get; set; }
+    }
+
+}
---
+++ POS-PointOfSale/POS-PointOfSale/Models/POS_PointOfSaleContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace POS_PointOfSale.Models
+{
+    public class POS_PointOfSaleContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public POS_PointOfSaleContext() : base("name=POS_PointOfSaleContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<POS_PointOfSale.Models.Cliente> Clientes { get; set; }
+    
+    }
+}
